775624058c5ad5d64378de1b20173e90f0583ade
Gragy08 <giaqui1712@gmail.com>
2025-06-18 17:30:57 +0700

    Hiển thị giao diện theo trạng thái Đăng nhập - FE

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+
+export const useAuth = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  const [infoUser, setInfoUser] = useState<any>();
+  const pathname = usePathname(); // Lấy URL hiện tại
+
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/check`, {
+      credentials: "include", // Gửi kèm cookie
+    })
+      .then(res => res.json())
+      .then(data => {
+        if(data.code == "error") {
+          setIsLogin(false);
+        }
+
+        if(data.code == "success") {
+          setIsLogin(true);
+          setInfoUser(data.infoUser);
+        }
+      });
+  }, [pathname]);
+
+  return { isLogin, infoUser };
+}
+
+// File để kiểm tra xem mỗi lần chuyển trang thì sẽ call lại API để kiểm tra trạng thái
